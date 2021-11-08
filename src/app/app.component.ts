8e6041b04a189897f9efb93b025857cef71bc47b
Julio César Guzmán Cortés <juliocgc10@hotmail.com>
2021-11-08 01:49:03 -0600

    Cambios dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice4.png';
+  number1: number = 1;
+  number2: number = 2;
+
+  tirarDados(): void {
+    this.number1 = Math.round(Math.random() * 5) + 1;
+    this.number2 = Math.round(Math.random() * 5) + 1
+    this.dadoIzquierdo = '../assets/img/dice' + this.number1 + '.png';
+    this.dadoDerecho = '../assets/img/dice' + this.number2 + '.png';    
+  }
 }
